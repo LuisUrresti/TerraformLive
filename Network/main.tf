b9bc82344fa669c8f27bc80a25e4f532c9d7a1a5
Luis Urresti De las alas Pumariño <lurresti@everis.com>
2021-01-21 16:05:27 +0100

    se actualiza network

--- Network/main.tf
+++ Network/main.tf
@@ -18,18 +18,20 @@ terraform {
 }
 
 module "network" {
-  source = "github.com/LuisUrresti/TerraformModules.git"
+  source = "github.com/LuisUrresti/TerraformModules.git//Network"
 
   #Variables
   rsg_name = "rsg_1"
   location = "northeurope"
   enviroment = "Repaso_1"
-  vnet_name = "vnet_1"
-  vnet_address = "10.10.0.0/16"
+  vnet_name_1 = "vnet_1"
+  vnet_address_1 = "10.10.0.0/16"
   snet_1 = "snet_1"
   snet_address_1 = "10.10.100.0/24"
   snet_2 = "snet_2"
   snet_address_2 = "10.10.200.0/24"
   sg1_name = "seg_group_1"
+  secrule1 = "ssh_rule"
   sg2_name = "seg_group_2"
+  secrule2 = "ssh_rule"
 }
