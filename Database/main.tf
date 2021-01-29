5fadef1e4d00fdddefbc2b1e3ac46808c8b2d0b3
Luis Urresti De las alas Pumariño <lurresti@everis.com>
2021-01-29 09:45:18 +0100

    se aagrega database

---
+++ Database/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+    features {
+      
+    }
+}
+
+terraform {
+  backend "azurerm" {
+      resource_group_name = "rsg_storage_account"
+      storage_account_name = "stacc7109"
+      container_name = "terraform"
+      key = "database/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "azurerm"
+  config = {
+    resource_group_name = "rsg_storage_account"
+    storage_account_name = "stacc7109"
+    container_name = "terraform"
+    key    = "network/terraform.tfstate"
+  }
+}
+
+module "database" {
+  source = "github.com/LuisUrresti/TerraformModules.git//Database"
+
+  #Variables
+  rsg_name = data.terraform_remote_state.network.outputs.rsg
+  location = "northeurope"
+  postgre_server_name = "PostgreSQLServer"
+  db_name = "db"
+}
